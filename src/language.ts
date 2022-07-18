ff066f78d072bc446b0cf2b7dd98c3d010e4eda8
Martin Castro <martin.castro>
2022-07-18 23:34:41 +0200

    Quick fix
    -

--- src/language.ts
+++ src/language.ts
@@ -8,25 +8,3 @@ export enum Language {
     EN,
     ES,
 }
-
-// This exception is raised when the User attempts to
-// render a book using an unsupported language.
-export class LanguageError extends Error {}
-
-// This class implements a language-aware interface for subclasses.
-// Classes inheriting from this class can support multiple languages.
-export abstract class WithLanguage {
-    private language: Language
-
-    // Language getter.
-    getLanguage() : Language {
-        return this.language
-    }
-
-    // Settings language or raising LanguageError.
-    setLanguage(language: Language) {
-        if (language === null)
-            throw new LanguageError('Invalid language')
-        this.language = language
-    }
-}
